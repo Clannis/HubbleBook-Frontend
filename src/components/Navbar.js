818a28c9e54904962c239024fcad1330e656b2d9
Clannis <Dodds1990@gmail.com>
2020-11-15 06:24:22 -0600

    add home page links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
     render() {
         return(
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                <div className=" btn navbar-brand">HubbleBook</div>
+                <Link to={'/articles'}><div className=" btn navbar-brand">HubbleBook</div></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <div className=" btn nav-link">Home</div>
+                            <Link to={'/articles'}><div className=" btn nav-link">Home</div></Link>
                         </li>
                     </ul>
                     <form className="form-inline mt-2mt-md-0">
