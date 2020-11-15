7bf896a470361f83b2951974b8faf4ffbcc9362d
Clannis <Dodds1990@gmail.com>
2020-11-15 14:21:40 -0600

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
-            <div className="container">
-                <span className="text-white">All articles are provided by <a href="http://hubblesite.org">hubblesite.org</a> </span>
+            <div className="container text-white">
+                <p className="float-right"><a href="#">Back to top</a></p>
+                <p>All articles are provided by <a href="http://hubblesite.org">hubblesite.org</a></p>
+                <p> 2020 James R. Dodds  ·  <a href="#">About Us</a></p>
+                <p></p>
             </div>
         </footer>
     )
