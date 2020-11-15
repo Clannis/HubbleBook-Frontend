aa45856fa4ab4b33313719595c76ce412e74658b
Clannis <Dodds1990@gmail.com>
2020-11-15 09:44:17 -0600

    add navigation drop down for mission selection

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,33 @@
 import { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-class Navbar extends Component {
-    render() {
-        return(
-            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                <Link to={'/articles'}><div className=" btn navbar-brand">HubbleBook</div></Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link to={'/articles'}><div className=" btn nav-link">Home</div></Link>
-                        </li>
-                    </ul>
-                    <form className="form-inline mt-2mt-md-0">
-                        <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
-        )
-    }
+const Navbar = () => {
+    return(
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <Link to={'/articles'}><div className=" btn navbar-brand">HubbleBook</div></Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link to={'/articles'}><div className=" btn nav-link">Home</div></Link>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Missions</a>
+                        <div className="dropdown-menu" aria-labelledby="dropdown03">
+                            <Link to={'/missions/hubble'}><div className="dropdown-item" href="">Hubble</div></Link>
+                            <Link to={'/missions/james_webb'}><div className="dropdown-item" href="#">James Webb</div></Link>
+                        </div>
+                    </li>
+                </ul>
+                <form className="form-inline mt-2mt-md-0">
+                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
