be78b9f9c28ab17265f55b696a480693111f895b
Clannis <Dodds1990@gmail.com>
2020-11-15 09:22:22 -0600

    add fetch article action to git

---
+++ src/actions/fetchArticle.js
@@ -0,0 +1,9 @@
+export default function fetchArticle(article_id) {
+    console.log("inside fetch: " + article_id)
+    return (dispatch) => {
+      dispatch({ type: 'START_GETTING_ARTICLE_REQUEST' });
+      fetch(`http://localhost:3000/articles/${article_id}`)
+        .then(response => response.json())
+        .then(article => dispatch({ type: 'ADD_ACTIVE_ARTICLE', article }));
+    };
+  }
