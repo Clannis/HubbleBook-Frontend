f0a37ddb3ff07998a4c3757bafc18b520f5e5e37
Clannis <Dodds1990@gmail.com>
2020-11-17 07:25:06 -0600

    fix link to top of page warning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container text-white">
-                <p className="float-right"><a href="#">Back to top</a></p>
+                <p className="float-right"><a href="#top">Back to top</a></p>
                 <p>All articles are provided by the <a href="http://hubblesite.org/api/documentation" target="blank">API</a> run by <a href="http://hubblesite.org" target="_blank">hubblesite.org</a>  <br/>2020 James R. Dodds  ·  <a href="/about">About Us</a></p>
                 
             </div>
