fec0dbbc038ebfc1f7c7bfde237b7f4bc78c9971
Clannis <Dodds1990@gmail.com>
2020-11-17 07:22:17 -0600

    remove stikcy footer and update footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 const Footer = () => {
     return (
-        <footer className="footer mt-auto py-3 bg-dark fixed-bottom">
+        <footer className="footer mt-auto py-3 bg-dark">
             <div className="container text-white">
                 <p className="float-right"><a href="#">Back to top</a></p>
-                <p>All articles are provided by the <a href="http://hubblesite.org/api/documentation" target="blank">API</a> run by <a href="http://hubblesite.org" target="_blank">hubblesite.org</a>.</p>
-                <p> 2020 James R. Dodds  ·  <a href="/about">About Us</a></p>
-                <p></p>
+                <p>All articles are provided by the <a href="http://hubblesite.org/api/documentation" target="blank">API</a> run by <a href="http://hubblesite.org" target="_blank">hubblesite.org</a>  <br/>2020 James R. Dodds  ·  <a href="/about">About Us</a></p>
+                
             </div>
         </footer>
     )
