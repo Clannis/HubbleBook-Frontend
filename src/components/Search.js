71b5498205a3cd048074d188be1a3935fafd759a
Clannis <Dodds1990@gmail.com>
2020-11-18 19:45:51 -0600

    set search for to a comtrolled component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,19 @@
 import { Component } from "react"
 
 class Search extends Component {
+
+    state = {
+        search: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
     render(){
         return (
             <form className="form-inline mt-2mt-md-0">
-                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+                <input className="form-control mr-sm-2" type="text" name="search" placeholder="Search" aria-label="Search" onChange={this.handleChange} value={this.state.search}/>
                 <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
             </form>
         )
