d9b92f8460c18a351a0d7cd04d38d53b4f5b4cd8
Clannis <Dodds1990@gmail.com>
2020-11-21 13:11:04 -0600

    update styling

--- src/containers/SearchCategoryContainer.js
+++ src/containers/SearchCategoryContainer.js
@@ -18,7 +18,7 @@ class SearchCategoryContainer extends Component {
     render() {
         return(
             <div className="container-fluid h-100 d-flex flex-column">
-                <div className="row flex-fill flex-grow-1 py-2 px-5">
+                <div className="row flex-fill flex-grow-1 p-2 px-5">
                     <div className="col-4 mh-100">
                         <h3 className="text-center">Results by<br/>Title</h3>
                     </div>
@@ -29,18 +29,18 @@ class SearchCategoryContainer extends Component {
                         <h3 className="text-center">Results by<br/>Users</h3>
                     </div>
                 </div>
-                <div className="row flex-fill flex-grow-1 py-2 px-5" style={{minHeight: 0}}>
-                    <div className="col mh-100 py-2 shadow-lg p-2 m-4" style={{overflowY: "scroll"}}>
+                <div className="row flex-fill flex-grow-1 px-5" style={{minHeight: 0}}>
+                    <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
                         <div className="row justify-content-center">
                             {this.renderArticlesByTitle()}
                         </div>
                     </div>
-                    <div className="col mh-100 py-2 shadow-lg p-2 m-4" style={{overflowY: "scroll"}}>
+                    <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
                         <div className="row justify-content-center">
                             {this.renderArticlesByContent()}
                         </div>
                     </div>
-                    <div className="col mh-100 py-2 shadow-lg p-2 m-4" style={{overflowY: "scroll"}}>
+                    <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
                         <div className="row justify-content-center">
                             {this.renderArticlesByContent()}
                         </div>
