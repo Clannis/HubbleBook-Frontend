3d5e095c6fce9c505d4bb8d022e2e580c730a61e
Clannis <Dodds1990@gmail.com>
2020-11-21 16:59:00 -0600

    add conditional loading image to searchCategoryContainer and adjust spacing

--- src/containers/SearchCategoryContainer.js
+++ src/containers/SearchCategoryContainer.js
@@ -24,39 +24,39 @@ class SearchCategoryContainer extends Component {
         if (this.props.requesting) {
             return(
                 <div className="container-fluid h-75 d-flex flex-column">
-                <div className="row flex-fill flex-grow-1 p-2 px-5">
-                    <div className="col-4 mh-100">
-                        <h3 className="text-center">Results by<br/>Title: {this.props.search.title ? this.props.search.title.length : 0}</h3>
+                    <div className="row flex-fill flex-grow-1 p-2 px-5">
+                        <div className="col-4 mh-100">
+                            <h3 className="text-center">Results by<br/>Title: {this.props.search.title ? this.props.search.title.length : 0}</h3>
+                        </div>
+                        <div className="col-4 mh-100">
+                            <h3 className="text-center">Results by<br/>Content: {this.props.search.content ? this.props.search.content.length : 0}</h3>
+                        </div>
+                        <div className="col-4 mh-100">
+                            <h3 className="text-center">Results by<br/>Users</h3>
+                        </div>
                     </div>
-                    <div className="col-4 mh-100">
-                        <h3 className="text-center">Results by<br/>Content: {this.props.search.content ? this.props.search.content.length : 0}</h3>
-                    </div>
-                    <div className="col-4 mh-100">
-                        <h3 className="text-center">Results by<br/>Users</h3>
+                    <div className="row flex-fill flex-grow-1 px-5" style={{minHeight: 0}}>
+                        <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
+                            <div className="row justify-content-center">
+                                <Loading/>
+                            </div>
+                        </div>
+                        <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
+                            <div className="row justify-content-center">
+                                <Loading/>
+                            </div>
+                        </div>
+                        <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
+                            <div className="row justify-content-center">
+                                <Loading/>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="row flex-fill flex-grow-1 px-5" style={{minHeight: 0}}>
-                    <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
-                        <div className="row justify-content-center">
-                            <Loading/>
-                        </div>
-                    </div>
-                    <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
-                        <div className="row justify-content-center">
-                            <Loading/>
-                        </div>
-                    </div>
-                    <div className="col mh-100 py-4 shadow-lg m-4" style={{overflowY: "scroll"}}>
-                        <div className="row justify-content-center">
-                            <Loading/>
-                        </div>
-                    </div>
-                </div>
-            </div>
             )
         } else {
             return(
-                <div className="container-fluid h-75 d-flex flex-column">
+                <div className="container-fluid h-100 d-flex flex-column">
                     <div className="row flex-fill flex-grow-1 p-2 px-5">
                         <div className="col-4 mh-100">
                             <h3 className="text-center">Results by<br/>Title: {this.props.search.title ? this.props.search.title.length : 0}</h3>
