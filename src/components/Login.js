14cf6e1f04923ef97b94a660207c367bf96f74ea
Clannis <Dodds1990@gmail.com>
2020-11-21 15:08:02 -0600

    update Login and Sign up forms to be controlled forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,45 @@
 import { Component } from 'react'
 
 class Login extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        rememberMe: false
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const { username, email, password, rememberMe } = this.state
+        const body = {username: username, password: password, email: email}
+        // fetch 
+        .then(response => {
+            if (!response.errors){
+              console.log(response)
+              // do something
+            } else {
+              alert(response.errors.message)
+            }
+          })
+    }
+
     render() {
         return(
             <div class="container h-100">
                 <div class="row justify-content-center align-self-center text-center">
-                    <form className="col-6">
+                    <form className="col-6" onSubmit={this.handleSubmit}>
                         <img className="mb-4 rounded" src="https://cdn.dribbble.com/users/1199444/screenshots/6546398/hubble_space_telescope.png" alt="" width="100" height="72"/>
                         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus=""/>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
+                        <input type="text" id="inputUsername" name="username" className="form-control" placeholder="Username" required="" autoFocus="" onChange={this.handleChange}/>
+                        <input type="email" id="inputEmail" name="email" className="form-control" placeholder="Email Address" required="" autoFocus="" onChange={this.handleChange}/>
+                        <input type="password" id="inputPassword" name="password" className="form-control" placeholder="Password" required="" onChange={this.handleChange}/>
                         <div className="checkbox mb-3">
                             <label>
-                                <input type="checkbox" value="remember-me"/> Remember me
+                                <input type="checkbox" name="rememberMe" value="remember-me" onChange={this.handleChange}/> Remember me
                             </label>
                         </div>
                         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,47 @@
 import { Component } from 'react'
 
 class Signup extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const { username, email, password, passwordConfirmation } = this.state
+        if (password === passwordConfirmation){
+          const body = {username: username, password: password, email: email}
+          // fetch
+          .then(response => {
+            if (!response.errors){
+                console.log(response)
+                // do something
+            } else {
+              alert(response.errors.message)
+            }
+          }) 
+        } else {
+          alert("Passwords do not match!")
+        }
+    }
+
     render() {
         return(
             <div class="container h-100">
                 <div class="row justify-content-center align-self-center text-center">
-                    <form className="col-6">
+                    <form className="col-6" onSubmit={this.handleSubmit}>
                         <img className="mb-4 rounded" src="https://cdn.dribbble.com/users/1199444/screenshots/6546398/hubble_space_telescope.png" alt="" width="100" height="72"/>
                         <h1 className="h3 mb-3 font-weight-normal">Sign Up</h1>
-                        <input type="text" id="inputEmail" name="username" className="form-control" placeholder="Username" required="" autoFocus=""/>
-                        <input type="email" id="inputEmail" name="email" className="form-control" placeholder="Email address" required="" autoFocus=""/>
-                        <input type="password" id="inputPassword" name="password" className="form-control" placeholder="Password" required=""/>
-                        <input type="password" id="inputPassword" name="passwordConfirmation" className="form-control" placeholder="Confirm Password" required=""/>
+                        <input type="text" id="inputUsername" name="username" className="form-control" placeholder="Username" required="" autoFocus="" onChange={this.handleChange}/>
+                        <input type="email" id="inputEmail" name="email" className="form-control" placeholder="Email address" required="" autoFocus="" onChange={this.handleChange}/>
+                        <input type="password" id="inputPassword" name="password" className="form-control" placeholder="Password" required="" onChange={this.handleChange}/>
+                        <input type="password" id="inputPasswordConfirmation" name="passwordConfirmation" className="form-control" placeholder="Confirm Password" required="" onChange={this.handleChange}/>
                         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
                     </form>
                 </div>
