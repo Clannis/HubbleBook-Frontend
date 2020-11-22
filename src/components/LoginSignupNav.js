7f47b81b5eee110148a227b8da2ef3de93bcb0c4
Clannis <Dodds1990@gmail.com>
2020-11-22 10:42:31 -0600

    add routing to login/signup navbar

--- src/components/LoginSignupNav.js
+++ src/components/LoginSignupNav.js
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom"
+
 const LoginSignupNav = (props) => {
     console.log(props.login)
     return props.login ?
      (
         <nav class="site-header fixed-top py-1 bg-light">
             <div class="container d-flex flex-column flex-md-row justify-content-between">
-                <a class="py-2 d-none btn btn-primary d-md-inline-block" href="#">Welcome</a>
-                <a class="py-2 d-none btn btn-primary d-md-inline-block" href="#">Signup</a>
+                <Link to={"/"} ><button class="py-2 d-none btn btn-primary d-md-inline-block">Welcome</button></Link>
+                <Link to={"/Signup"} ><button class="py-2 d-none btn btn-primary d-md-inline-block">Sign Up</button></Link>
             </div>
         </nav>
     ) :
      (
         <nav class="site-header fixed-top py-1 bg-light">
             <div class="container d-flex flex-column flex-md-row justify-content-between">
-                <a class="py-2 d-none btn btn-primary d-md-inline-block" href="#">Welcome</a>
-                <a class="py-2 d-none btn btn-primary d-md-inline-block" href="#">Login</a>
+                <Link to={"/"} ><button class="py-2 d-none btn btn-primary d-md-inline-block">Welcome</button></Link>
+                <Link to={"/Login"} ><button class="py-2 d-none btn btn-primary d-md-inline-block">Log In</button></Link>
             </div>
         </nav>
     )
