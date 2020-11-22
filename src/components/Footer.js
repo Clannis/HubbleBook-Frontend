c6ba21d6a430f81e5b3e296f3eb8aa8b8c44dcdf
Clannis <Dodds1990@gmail.com>
2020-11-22 14:38:39 -0600

    add dot to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark fixed-bottom position-relative">
             <div className="container text-white d-flex justify-content-between">
-                <p>All articles are provided by the <a href="http://hubblesite.org/api/documentation" target="blank">API</a> run by <a href="http://hubblesite.org" target="_blank" rel="noreferrer">hubblesite.org</a> </p><p>2020 James R. Dodds  ·  <a href="/about">About Us</a></p>   
+                <p>All articles are provided by the <a href="http://hubblesite.org/api/documentation" target="blank">API</a> run by <a href="http://hubblesite.org" target="_blank" rel="noreferrer">hubblesite.org</a> </p><p>2020  ·  James R. Dodds  ·  <a href="/about">About Us</a></p>   
             </div>
         </footer>
     )
