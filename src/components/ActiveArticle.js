4336083e454779e7e6a1f687e35c05e9d2565c60
Clannis <Dodds1990@gmail.com>
2020-11-22 21:43:17 -0600

    reomve shadow from coment container

--- src/components/ActiveArticle.js
+++ src/components/ActiveArticle.js
@@ -44,7 +44,7 @@ class ActiveArticle extends Component {
                         </div>
 
                         <div className="row">
-                            <div className="col my-3 p-3 bg-white rounded shadow-sm">
+                            <div className="col my-3 p-3 bg-white">
                                 <h6 className="border-bottom border-gray pb-2 mb-0">Comments</h6>
                                 <div className="media text-muted pt-3">
                                     <svg className="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
