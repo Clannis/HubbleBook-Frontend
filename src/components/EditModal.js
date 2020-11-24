47f29470f9e846e4be27778c627261268324566d
Clannis <Dodds1990@gmail.com>
2020-11-24 15:55:18 -0600

    add submit input to form to allow form submition on enter key and remove modal upon submition

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -8,12 +8,7 @@ import updateCommentContent from '../actions/updateCommentContent'
 class EditModal extends Component {
     constructor() {
         super()
-        
-        this.formContent = createRef()  
-    }
-
-    componentDidMount() {
-         
+        this.modal = createRef()  
     }
 
     handleChange = (event) => {
@@ -21,15 +16,19 @@ class EditModal extends Component {
         
     }
 
-    handleSubmit = () => {
-        console.log(this.props.content)
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(event)
+        this.modal.current.style = {display: "none"}
+        let backdrop = document.querySelector('.modal-backdrop')
+        backdrop.remove()
         let data = { content: this.props.content, commentId: this.props.commentId }
         this.props.updateCommentContent(data)
     }
 
     render() {
         return (
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div ref={this.modal} className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
@@ -38,19 +37,20 @@ class EditModal extends Component {
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <form>
-                            <div className="modal-body">
-                                
-                                    <div className="input-group" id="show_hide_password">
-                                        <input ref={this.formContent} type="text" id="content" name="content" className="form-control" required="" value={this.props.content} onChange={this.handleChange}/>
-                                    </div>
-                                
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => this.props.clearModalContent()}>Close</button>
-                                <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Save changes</button>
-                            </div>
-                        </form>
+                        
+                        <div className="modal-body">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="input-group" id="show_hide_password">
+                                    <input type="text" id="content" name="content" className="form-control" required="" value={this.props.content} onChange={this.handleChange}/>
+                                </div>
+                                <input type="submit" value="Login!" style={{display: "none"}}/>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => this.props.clearModalContent()}>Close</button>
+                            <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.handleSubmit}>Save changes</button>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
