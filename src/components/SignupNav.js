33be7c6875e9055ed9ed6d0322af70eae72c069c
Clannis <Dodds1990@gmail.com>
2020-11-23 21:36:00 -0600

    remove welcome link and replace page brand link path to welcome page

--- src/components/SignupNav.js
+++ src/components/SignupNav.js
@@ -3,15 +3,12 @@ import { Link } from "react-router-dom"
 const SignupNav = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
-            <Link to={'/articles'}><div className=" btn navbar-brand"><img src="https://static.thenounproject.com/png/331709-200.png" alt="icon" height="30px" width="30px" />HubbleBook</div></Link>
+            <Link to={'/'}><div className=" btn navbar-brand"><img src="https://static.thenounproject.com/png/331709-200.png" alt="icon" height="30px" width="30px" />HubbleBook</div></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to={"/"} ><div className="btn nav-link">Welcome</div></Link>
-                    </li>
                     <li className="nav-item">
                         <Link to={"/Login"} ><div className="btn nav-link">Log In</div></Link>
                     </li>
